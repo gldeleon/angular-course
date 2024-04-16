5e39448bf37501f17c548fb1c55fd4bef9a45a6f
Geovanny De Leon <gldeleon@live.com.mx>
2024-04-16 17:29:22 -0600

    feature: typescript video 17 merge

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -12,12 +12,20 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
     return firstNumber * base;
 }
 
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => 
+}
 
-const result = addNumbers(1,2);
-const resultArrow = addNumbersArrow(6,9);
-const multiplyResult: number = multiply(3,4);
+// const result = addNumbers(1,2);
+// const resultArrow = addNumbersArrow(6,9);
+// const multiplyResult: number = multiply(3,4);
 
-console.log({result, resultArrow, multiplyResult});
+// console.log({result, resultArrow, multiplyResult});
 
+const healCharacter = (character, amount: number) => {
+    character.pv += amount;
+}
 
 export {};
